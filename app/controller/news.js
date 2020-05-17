8f8c69685f8e1dc47edc59fd67d4ced2f9e6f4b3
runningsparrow <runningsparrow@gmail.com>
2020-05-17 23:16:03 +0800

    text

---
+++ app/controller/news.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class NewsController extends Controller {
+  async index() {
+    // const { ctx } = this;
+    // ctx.body = 'hi, egg';
+
+    this.ctx.body = "新闻页面";
+  }
+
+  async content() {
+
+    //获取 get 传值
+
+
+    // koa中如何获取get传值  ctx.query
+
+    //egg.js 里面获取
+
+    var query = this.ctx.query;
+
+    console.log(query);
+
+    this.ctx.body = "新闻详情";
+  }
+
+  async newslist(){
+
+    //koa获取动态路由传值  ctx.params
+
+    var params = this.ctx.params;
+    console.log(params);
+
+    this.ctx.body="新闻列表";
+
+  }
+
+}
+
+
+
+
+module.exports = NewsController;
