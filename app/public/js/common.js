aacf5340466f487e1e46820bcfb9868c10e3d80a
runningsparrow <runningsparrow@gmail.com>
2020-06-02 06:45:38 +0800

    0601

--- app/public/js/common.js
+++ app/public/js/common.js
@@ -1,36 +1,119 @@
-var tree = [
-    {
-      text:"Parent 1",
-      nodes: [
-        {
-          text:"Child 1",
-          nodes: [
-            {
-              text:"Grandchild 1"
-            },
-            {
-              text:"Grandchild 2"
-            }
-          ]
-        },
-        {
-          text:"Child 2"
-        }
-      ]
-    },
-    {
-      text:"Parent 2"
-    },
-    {
-      text:"Parent 3"
-    },
-    {
-      text:"Parent 4"
-    },
-    {
-      text:"Parent 5"
-    }
-  ];                 
+var tree = [{
+	"nodes": [{
+		"nodes": [{
+			"nodes": [{
+				"level": "4",
+				"id": "4",
+				"text": "天猫超市",
+				"parentid": "3"
+			}, {
+				"level": "4",
+				"id": "5",
+				"text": "个人店铺",
+				"parentid": "3"
+			}],
+			"level": "3",
+			"id": "3",
+			"text": "淘宝商城",
+			"parentid": "2"
+		}],
+		"level": "2",
+		"id": "2",
+		"text": "电商平台",
+		"parentid": "1"
+	}, {
+		"nodes": [{
+			"level": "3",
+			"id": "7",
+			"text": "支付宝",
+			"parentid": "6"
+		}],
+		"level": "2",
+		"id": "6",
+		"text": "支付平台",
+		"parentid": "1"
+	}, {
+		"nodes": [{
+			"level": "3",
+			"id": "9",
+			"text": "虾米音乐",
+			"parentid": "8"
+		}],
+		"level": "2",
+		"id": "8",
+		"text": "音乐平台",
+		"parentid": "1"
+	}],
+	"level": "1",
+	"id": "1",
+	"text": "阿里巴巴",
+	"parentid": "0"
+}, {
+	"nodes": [{
+		"nodes": [{
+			"level": "3",
+			"id": "12",
+			"text": "英雄联盟",
+			"parentid": "11"
+		}, {
+			"level": "3",
+			"id": "13",
+			"text": "地下城与勇士",
+			"parentid": "11"
+		}, {
+			"level": "3",
+			"id": "14",
+			"text": "穿越火线",
+			"parentid": "11"
+		}],
+		"level": "2",
+		"id": "11",
+		"text": "游戏平台",
+		"parentid": "10"
+	}, {
+		"nodes": [{
+			"level": "3",
+			"id": "16",
+			"text": "腾讯QQ",
+			"parentid": "15"
+		}, {
+			"nodes": [{
+				"level": "4",
+				"id": "18",
+				"text": "个人微信",
+				"parentid": "17"
+			}, {
+				"level": "4",
+				"id": "19",
+				"text": "企业微信",
+				"parentid": "17"
+			}],
+			"level": "3",
+			"id": "17",
+			"text": "微信",
+			"parentid": "15"
+		}],
+		"level": "2",
+		"id": "15",
+		"text": "聊天平台",
+		"parentid": "10"
+	}, {
+		"nodes": [{
+			"level": "3",
+			"id": "21",
+			"text": "QQ音乐",
+			"parentid": "20"
+		}],
+		"level": "2",
+		"id": "20",
+		"text": "音乐平台",
+		"parentid": "10"
+	}],
+	"level": "1",
+	"id": "10",
+	"text": "腾讯科技",
+	"parentid": "0"
+}]
   
 
   function getTree() {
@@ -40,4 +123,52 @@ var tree = [
 }
 
 
-$('#tree').treeview({data: getTree()});       
+$('#tree').treeview({
+  data: getTree(),
+  color:"#428bca",
+  levels:3,
+  onNodeSelected:function(event,node){
+    console.log("选中了"+ node.text);
+
+    var select_node = $(this).treeview('getSelected');
+    // console.log(select_node[0].text)
+
+    console.log(select_node)
+
+    $(this).treeview('expandNode',select_node);
+
+    // console.log(select_node[0].state)
+    
+    // if (select_node[0].state.expanded) {
+    //   $(this).treeview('collapseNode', select_node);
+    //   // select_node[0].state.selected = false;
+    // }
+    // else {
+    //   $(this).treeview('expandNode', select_node);
+    //   // select_node[0].state.selected = true;
+      
+    // }
+    
+  },
+  onNodeUnselected:function(event,node){
+    console.log("取消了"+ node.text);
+
+    var unselect_node = $(this).treeview('getUnselected');
+
+    console.log(unselect_node)
+
+    $(this).treeview('collapseNode',unselect_node);
+
+
+    // var nodelist = [node]
+
+    // console.log(nodelist)
+
+
+
+    // $(this).treeview('expandNode', nodelist);
+    
+    
+
+  }
+});       
