22c5579e0dadd240a4a93a52c8a3fd434c699f58
runningsparrow <runningsparrow@gmail.com>
2020-06-14 12:39:27 +0800

    0615

--- app/controller/upfile.js
+++ app/controller/upfile.js
@@ -6,7 +6,18 @@ class UpfileController extends Controller {
 
     async index() {
      
-        await this.ctx.render('upfile');
+        // await this.ctx.render('upfile');
+
+        var msg = "successful"
+        var returncode = 0
+        const doconfigall = await this.ctx.service.doconfig.findall();
+        await console.log(doconfigall)
+        await this.ctx.render('upfile',{
+        doconfigall: doconfigall,
+        msg: msg,
+        returncode:returncode
+        });
+
         
     }
 }
---
+++ app/public/js/upfile1.js
@@ -0,0 +1,4 @@
+// var select = $("#slpk");
+// select.append("<option value='1'>香蕉</option>");
+// select.append("<option value='2'>苹果</option>");
+// select.append("<option value='3'>橘子</option>");
